d47aa0721cd52505c026579e36e1a227d23210ce
Antosha <anton.perebyinis@gmail.com>
2015-02-10 19:11:12 +0000

    Add size to gravatar(exercise#1)

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,8 +1,9 @@
 module UsersHelper
   # returns Gravatar for a given User
-  def gravatar_for(user)
+  def gravatar_for(user, options = { size: 80 })
     gravatar_md5 = Digest::MD5::hexdigest(user.email.downcase)
-    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_md5}"
+    size = options[:size]
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_md5}?s=#{size}"
     image_tag(gravatar_url, alt: user.name, class: 'gravatar')
   end
 end
